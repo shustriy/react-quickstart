b378086b7ccbc29676ad3b2e2d7e1f02cbce1825
Andrey Kotelnikov <kotelnikov.andrey@viawolf.net>
2025-01-27 12:41:53 +0300

    Fill cell by cross. Replaced displaying numbers to crosses.

--- frontend/source/src/Square.js
+++ frontend/source/src/Square.js
@@ -1,7 +1,12 @@
-export default function Square({value}) {
+import { useState } from "react"
+
+export default function Square() {
+
+    const [value, setValue] = useState(null);
 
     function handleClick() {
         console.log('clicked!')
+        setValue('X');
     }
     return <button className="square" onClick={handleClick}>{value}</button>
 }
